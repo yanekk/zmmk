ebeb0d20587578a3a747fb3eca352ec166c6b7a7
Jan Krolikowski <yanekk86@gmail.com>
2010-07-16 12:39:53 +0200

    Corrected css, js and title inclusion methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,49 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   layout "default"
+  before_filter do |controller|
+    controller.init_stylesheets
+    controller.init_title
+    controller.init_javascripts
+  end
+
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  helper_method :include_stylesheet, :include_javascript, :add_to_title
+
+  def include_stylesheet(filename)
+    @@css << filename
+  end
+
+  def include_javascript(filename)
+    @@js << filename
+  end
+
+  def add_to_title(fragment)
+    @@title << fragment
+  end
+
+  def init_title
+    @@title = []
+  end
+  def init_javascripts
+    @@js  = []
+  end
+  def init_stylesheets
+    @@css = []
+  end
+
+  def self.javascripts
+    @@js
+  end
+  def self.stylesheets
+    @@css
+  end
+  def self.title
+    @@title
+  end
+
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,39 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def include_stylesheet(filename)
-    @@css ||= []
-    @@css << filename
-  end
-  def render_stylesheets
-    @@css ||= []
-    @@css.map {|css| stylesheet_link_tag(css)}.join("\n")
-  end
 
-  def include_javascript(filename)
-    @@js ||= []
-    @@js << filename
+  def render_stylesheets
+    ApplicationController.stylesheets.map {|css| stylesheet_link_tag(css)}.join("\n")
   end
 
   def render_javascripts
-    @@js ||= []
-    @@js.map {|js| javascript_include_tag(js)}.join("\n")
-  end
-
-  def add_to_title(fragment)
-    init_title
-    @@title << fragment
+    ApplicationController.javascripts.map {|js| javascript_include_tag(js)}.join("\n")
   end
 
   def render_title(separator = " - ")
-    init_title
-    return @@title.join(separator)
+    ApplicationController.title.join(separator)
   end
-
-  private
-  def init_title
-    @@title ||= []
-  end
-
 
 end
 
