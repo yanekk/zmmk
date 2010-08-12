981ca8082edd2b4649f5cdfca2b3b2349cf84289
Jan Krolikowski <yanekk86@gmail.com>
2010-08-12 13:33:18 +0200

    Corrected Mime handling

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-Mime::Type.register "text/css", :css
-Mime::Type.register "application/javascript", :js
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -45,4 +42,7 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem "haml"
 end
+
+Mime::Type.register "text/css", :css
+Mime::Type.register "application/javascript", :js
 
