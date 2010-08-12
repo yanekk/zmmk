4146da2cfb9e330f6f3b821b69a802fe3757e403
Jan Krolikowski <yanekk86@gmail.com>
2010-08-12 13:24:15 +0200

    Mime types declarations

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
+Mime::Type.register "text/css", :css
+Mime::Type.register "application/javascript", :js
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
